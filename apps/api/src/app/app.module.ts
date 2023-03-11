7eb5f182d2bd24ade4d0fccd7671fa67c4760bf8
Tyler Bauman <t.bauman@vehikl.com>
2023-03-11 14:47:03 -0500

    chore: fix path aliases

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common'
 
 import { AppController } from '@controllers/app.controller'
+import { UserController } from '@controllers/user.controller'
+import { DrinkController } from '@controllers/drink.controller'
+
 import { AppService } from '@services/app.service'
 import { UserService } from '@services/user.service'
-import { UserController } from '@controllers/user.controller'
 import { DrinkService } from '@services/drink.service'
-import { DrinkController } from '@controllers/drink.controller'
+import { PrismaService } from '@services/prisma.service'
 
 @Module({
   imports: [],
   controllers: [AppController, UserController, DrinkController],
-  providers: [AppService, UserService, DrinkService]
+  providers: [AppService, PrismaService, UserService, DrinkService]
 })
 export class AppModule {}
