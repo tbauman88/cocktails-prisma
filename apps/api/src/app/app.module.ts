eb77c337fa775470775126d2a9e41e50a7cef509
Tyler Bauman <t.bauman@vehikl.com>
2023-03-11 13:52:39 -0500

    add prisma service, user service/controller

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AppController } from '@controllers/app.controller'
+import { AppService } from '@services/app.service'
+import { UserService } from '@services/user.service'
+import { UserController } from '@controllers/user.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService]
 })
 export class AppModule {}
---
+++ apps/api/src/app/controllers/user.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Delete, Post, Put } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
+import { Prisma, User } from '@prisma/client'
+import { UserService } from '../services/user.service'
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('user/:id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    return this.userService.show({ id })
+  }
+
+  @Get('users')
+  async getUsers(): Promise<User[]> {
+    return this.userService.index({})
+  }
+
+  @Post('user')
+  async createUser(@Body() data: Prisma.UserCreateInput): Promise<User> {
+    return this.userService.create(data)
+  }
+
+  @Put('user/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: Prisma.UserUpdateInput
+  ): Promise<User> {
+    return this.userService.update({ where: { id }, data })
+  }
+
+  @Delete('user/:id')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return this.userService.delete({ id })
+  }
+}
