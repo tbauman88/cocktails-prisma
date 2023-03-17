89b1fb18af7a0552371898099d39e5b846318e87
Tyler Bauman <t.bauman@vehikl.com>
2023-03-17 12:04:05 -0400

    use local prisma with dockerized postgres db

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,21 +3,26 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from '@api/app.module'
 import { PrismaService } from '@api/services/prisma.service'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 3333
-  await app.listen(port)
 
-  // const prismaService = app.get(PrismaService)
-  // await prismaService.enableShutdownHooks(app)
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
+
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(port)
 
   Logger.log(`🚀 Application is running on: http://localhost:${port}`)
 }
