f4c0278874b63feb20d24378385ed0faf4a49b4a
Tyler Bauman <t.bauman@vehikl.com>
2023-03-22 16:59:48 -0400

    update api coverage
    Signed-off-by: Tyler Bauman <t.bauman@vehikl.com>

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -2,10 +2,12 @@
 export default {
   displayName: 'api',
   preset: '../../jest.preset.js',
+  verbose: true,
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api'
+  coverageDirectory: '../../coverage/apps/api',
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]]
 }
