11c3aa4d4f2a05a9e30c3669e4931d65bea3df3c
Tyler Bauman <t.bauman@vehikl.com>
2023-04-14 12:34:08 -0400

    update ci
    Signed-off-by: Tyler Bauman <t.bauman@vehikl.com>

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -3,15 +3,14 @@ import type { Config } from 'jest'
 const config: Config = {
   displayName: 'api',
   preset: '../../jest.preset.js',
-  verbose: true,
   testEnvironment: 'node',
-  testTimeout: 10000,
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api',
-  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]]
+  // coverageDirectory: '../../coverage/apps/api',
+  // coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
+  setupFilesAfterEnv: ['./jest.setup.ts']
 }
 
 export default config
